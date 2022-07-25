e54c58f3df931e66d943211fe6941286a4c4b371
Yung Visal <yungvisal@gmail.com>
2022-07-25 16:01:04 +0700

    first commit

---
+++ Hello_world.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP test on Heroku</title>
+</head>
+<body>
+<?php
+// PHP code goes here
+echo" Hello This is visal, i am 22 years old, i'm from Preyveng."
+?>
+</body>
+</html>
